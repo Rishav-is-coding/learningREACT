6e1a3d5a7b8a1c38aafc2feec2fc27c6e86da334
Rishav-is-coding <rishavraj02012003@gmail.com>
2025-05-28 15:35:18 +0530

    update

--- 04bgchanger/src/App.jsx
+++ 04bgchanger/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
       <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2" >
           <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-2xl" >
             <button 
-            onClick={() => setColor("red")}
+            onClick={() => setColor("red")} //"onClick" expects a function -> onClick={setColor('red')} -> then its getting return value of setColor function
             className="outline-none px-4 py-1 rounded-full" style={{backgroundColor: "red"}}
             >Red</button>
 
